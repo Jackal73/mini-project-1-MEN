b1ef563e8f84ba96718619b8c7a928b06e1de046
Shawn Kebel <fxrender@outlook.com>
2021-10-26 11:38:15 -0500

    Get all sample_airbnb data

--- server/app/router.js
+++ server/app/router.js
@@ -1,10 +1,21 @@
 import Router from "express";
+import config from "./config.js";
+import client from "./db/connections/client.js";
+
+const collection = client.db(config.db.name).collection(config.db.collection);
 
 const router = new Router();
 
 // localhost:3000/api
 router.get("/", (_, res) => {
   res.send("Hello World from API router!");
+});
+
+// localhost:3000/api/sample_airbnb
+router.get("/sample_airbnb", async (_, res) => {
+  // Get all of the sample_airbnb
+  const sampleAirbnb = await collection.find().toArray();
+  res.json(sampleAirbnb);
 });
 
 export default router;
