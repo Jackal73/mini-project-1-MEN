41cfaa73b41a797533c1a9eee2ae33d8f7b83a0a
Shawn Kebel <fxrender@outlook.com>
2021-11-07 01:55:40 -0500

    Add route for testing

--- server/app/router.js
+++ server/app/router.js
@@ -6,23 +6,8 @@ const collection = client.db(config.db.name).collection(config.db.collection);
 
 const router = new Router();
 
-// localhost:3000/api
+// Test - Get hello world
 router.get("/", (_, res) => {
   res.send("Hello World from API router!");
-});
-
-// localhost:3000/api/sample_airbnb
-router.get("/sample_airbnb", async (_, res) => {
-  // Get all of the sample_airbnb
-  const sampleAirbnb = await collection.find().limit(2).toArray();
-  res.json(sampleAirbnb);
-});
-
-// localhost:3000/api/sample_airbnb/current listings
-
-// localhost:3000/api/sample_airbnb/:id
-router.get("/sample_airbnb/:id", async (req, res) => {
-  const airbnbListing = await collection.findOne({ _id: req.body.id });
-  res.json(airbnbListing);
 });
 export default router;
